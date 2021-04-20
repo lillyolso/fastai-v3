90688cffffbc78f2428ffde1231dabeed1dbf5cc
Lilly Olson <33756865+lillyolso@users.noreply.github.com>
2021-04-20 10:33:21 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -78,12 +78,10 @@ def get_confirm_token(response):
     for key, value in response.cookies.items():
         if key.startswith('download_warning'):
             return value
-
     return None
 
 def save_response_content(response, destination):
     CHUNK_SIZE = 32768
-
     with open(destination, "wb") as f:
         for chunk in response.iter_content(CHUNK_SIZE):
             if chunk: # filter out keep-alive new chunks
